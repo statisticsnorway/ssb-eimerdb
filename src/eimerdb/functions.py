7236cae87b366d9028c64793065b0399e23f905a
StianElisenberg <stian.elisenberg@ssb.no>
2023-09-16 17:06:03 +0200

    More docstrings

--- src/eimerdb/functions.py
+++ src/eimerdb/functions.py
@@ -62,63 +62,91 @@ def arrow_schema_from_json(json_schema):
         Pyarrow schema.
 
     """
-    schema_dict = json.loads(json_schema)
-
-    schema_fields = schema_dict.get("table_schema", [])
-
-    arrow_fields = []
-    for field in schema_fields:
-        name = field["name"]
-        type_str = field["type"]
-        label = field["label"]
-
-        if type_str == "string":
-            arrow_type = pa.string()
-        elif type_str == "int64":
-            arrow_type = pa.int64()
-        else:
-            raise ValueError(f"Unsupported data type: {type_str}")
-        arrow_field = pa.field(
-            name, arrow_type, nullable=True, metadata={"label": label}
-        )
-        arrow_fields.append(arrow_field)
-    arrow_schema = pa.schema(arrow_fields)
-    return arrow_schema
+    fields = []
+    for field_dict in json_schema:
+        name = field_dict["name"]
+        data_type = field_dict["type"]
+        label = field_dict["label"]
+        field_type = getattr(
+            pa, data_type
+        )()  # Use PyArrow data type from "type" field
+        metadata = {"label": label}  # Include label as metadata
+        field = pa.field(name, field_type, metadata=metadata)
+        fields.append(field)
+    return pa.schema(fields)
 
 
 def parse_sql_query(sql_query):
-    select_pattern = r'^SELECT\s+(.*?)\s+FROM\s+(\w+)(?:\s+(.*))?$'
-    update_pattern = r'^UPDATE\s+(\w+)\s+SET\s+(.*?)\s+(?:WHERE\s+(.*))?$'
-    
-    select_match = re.match(select_pattern, sql_query, re.IGNORECASE)
-    update_match = re.match(update_pattern, sql_query, re.IGNORECASE)
+    """A function that parses the given sql query.
+
+     Args:
+        sql_query: An sql query.
+
+    Returns:
+        Dictionary with keys: Operation, columns, table_name and sql_filter.
+
+    """
+    select_pattern = r"^SELECT\s+(.*?)\s+FROM\s+(\w+)(?:\s+(.*))?$"
+    update_pattern = (
+        r"^UPDATE\s+(\w+)\s+SET\s+(.*?)\s+(?:WHERE\s+(.*))?$"
+    )
+
+    select_match = re.match(
+        select_pattern, sql_query, re.IGNORECASE
+    )
+    update_match = re.match(
+        update_pattern, sql_query, re.IGNORECASE
+    )
 
     if select_match:
         groups = select_match.groups()
         columns_str, table_name, rest_of_query = groups
-        columns = [re.sub(r'^COUNT\((.*?)\)$', r'\1', col.strip().split(' AS ')[0]) for col in columns_str.split(',')]
-        
+        columns = [
+            re.sub(
+                r"^COUNT\((.*?)\)$",
+                r"\1",
+                col.strip().split(" AS ")[0],
+            )
+            for col in columns_str.split(",")
+        ]
+
         return {
-            'operation': 'SELECT',
-            'columns': columns,
-            'table_name': table_name,
-            'sql_filter': rest_of_query.strip() if rest_of_query else None
+            "operation": "SELECT",
+            "columns": columns,
+            "table_name": table_name,
+            "sql_filter": rest_of_query.strip()
+            if rest_of_query
+            else None,
         }
     elif update_match:
         groups = update_match.groups()
         table_name, set_clause, where_clause = groups
 
         return {
-            'operation': 'UPDATE',
-            'table_name': table_name,
-            'set_clause': set_clause,
-            'where_clause': where_clause.strip() if where_clause else None
+            "operation": "UPDATE",
+            "table_name": table_name,
+            "set_clause": set_clause,
+            "where_clause": where_clause.strip()
+            if where_clause
+            else None,
         }
     else:
-        raise ValueError("Unsupported SQL operation. Only SELECT and UPDATE statements are allowed.")
+        raise ValueError(
+            "Unsupported SQL operation. Only SELECT and UPDATE statements are allowed."
+        )
 
 
 def create_eimerdb(bucket_name, db_name):
+    """Creates an EimerDB instance.
+
+     Args:
+        bucket_name: A google cloud storage bucket.
+        db_name: Name of the instance.
+
+    Returns:
+        success or failure
+
+    """
     creator = get_initials()
     token = AuthClient.fetch_google_credentials()
     client = storage.Client(credentials=token)
@@ -150,18 +178,30 @@ def create_eimerdb(bucket_name, db_name):
 
     role_groups = {
         "role_groups": {
-            "admin": {"operations": "all", "functions": "all", "tables": "all"},
+            "admin": {
+                "operations": "all",
+                "functions": "all",
+                "tables": "all",
+            },
             "editor": {
-                "operations": ["insert", "delete", "update", "reset"],
+                "operations": [
+                    "insert",
+                    "delete",
+                    "update",
+                    "reset",
+                ],
                 "functions": "",
                 "tables": "",
             },
         }
     }
 
-    role_groups_blob = bucket.blob(f"{full_path}/config/role_groups.json")
+    role_groups_blob = bucket.blob(
+        f"{full_path}/config/role_groups.json"
+    )
     role_groups_blob.upload_from_string(
-        data=json.dumps(role_groups), content_type="application/json"
+        data=json.dumps(role_groups),
+        content_type="application/json",
     )
 
     tables = {}
@@ -199,4 +239,3 @@ def example_function(number1: int, number2: int) -> str:
         return f"{number1} is less than {number2}"
     else:
         return f"{number1} is greater than or equal to {number2}"
-
--- src/eimerdb/instance.py
+++ src/eimerdb/instance.py
@@ -3,12 +3,9 @@ from functions import (
     get_initials,
     get_json,
     arrow_schema_from_json,
-    parse_sql_query,
-    create_eimerdb,
+    parse_sql_query
 )
 from dapla import FileClient, AuthClient
-from datetime import datetime
-import os
 from google.cloud import storage
 import json
 import pyarrow as pa
@@ -19,7 +16,60 @@ import pandas as pd
 
 
 class EimerDBInstance:
+    """
+    Represents an instance of the EimerDB database.
+
+    This class provides methods to interact with EimerDB, including
+    managing users, creating tables, inserting data, and querying data.
+
+    Args:
+        bucket_name (str): The name of the Google Cloud Storage bucket
+            where EimerDB is hosted.
+        eimer_name (str): The name of the EimerDB instance.
+
+    Attributes:
+        bucket (str): The name of the Google Cloud Storage bucket.
+        eimerdb_name (str): The name of the EimerDB instance.
+        path (str): The path to the EimerDB configuration.
+        eimer_path (str): The path to the EimerDB instance.
+        created_by (str): The name of the user who created the EimerDB instance.
+        time_created (str): The timestamp when the EimerDB instance
+            was created.
+        users (dict): A dictionary of EimerDB users and their roles (admin, user).
+        role_groups (dict): A dictionary of role groups and their members.
+        is_admin (bool): Indicates whether the current user is an admin.
+
+    Methods:
+        add_user(username, role): Add a user to EimerDB with a specified role.
+        remove_user(username): Remove a user from EimerDB.
+        create_table(table_name, schema, partition_columns=None,
+            editable=True): Create a new table in EimerDB.
+        main_table_insert(table_name, df): Insert data into the main table
+            of a specified schema.
+        query(sql_query, partition_select=None, unedited=False): Execute an SQL
+            query on a table in EimerDB.
+
+    """
     def __init__(self, bucket_name, eimer_name):
+        """
+        Initialize EimerDBInstance.
+
+        Args:
+            bucket_name (str): Name of GCS bucket.
+            eimer_name (str): Name of EimerDB instance.
+
+        Attributes:
+            bucket (str): GCS bucket name.
+            eimerdb_name (str): EimerDB instance name.
+            path (str): Configuration path.
+            eimer_path (str): Instance path.
+            created_by (str): Creator's name.
+            time_created (str): Creation timestamp.
+            users (dict): EimerDB users and roles.
+            role_groups (dict): Role groups.
+            is_admin (bool): Admin status.
+
+        """
         self.bucket = bucket_name
 
         about_path = f"eimerdb/{eimer_name}/config/about.json"
@@ -54,7 +104,18 @@ class EimerDBInstance:
             self.is_admin = False
 
     def add_user(self, username, role):
-        if self.is_admin == True:
+        """
+        Add a user with a specified role.
+
+        Args:
+            username (str): Name of the user to add.
+            role (str): Role to assign (admin or user).
+
+        Raises:
+            Exception: If the user is not an admin or the user already exists.
+
+        """
+        if self.is_admin is True:
             token = AuthClient.fetch_google_credentials()
             client = storage.Client(credentials=token)
             bucket = client.bucket(self.bucket)
@@ -62,7 +123,9 @@ class EimerDBInstance:
             new_user = {username: role}
             if username not in users:
                 users.update(new_user)
-                user_roles_blob = bucket.blob(f"{self.eimer_path}/config/users.json")
+                user_roles_blob = bucket.blob(
+                    f"{self.eimer_path}/config/users.json"
+                )
                 user_roles_blob.upload_from_string(
                     data=json.dumps(users), content_type="application/json"
                 )
@@ -73,14 +136,26 @@ class EimerDBInstance:
             raise Exception("Cannot add user. You are not an admin!")
 
     def remove_user(self, username):
-        if self.is_admin == True:
+        """
+        Remove a user by their username.
+
+        Args:
+            username (str): Name of the user to remove.
+
+        Raises:
+            Exception: If the user is not an admin or the user does not exist.
+
+        """
+        if self.is_admin is True:
             token = AuthClient.fetch_google_credentials()
             client = storage.Client(credentials=token)
             bucket = client.bucket(self.bucket)
             users = self.users
             if username in users:
                 del users[username]
-                user_roles_blob = bucket.blob(f"{self.eimer_path}/config/users.json")
+                user_roles_blob = bucket.blob(
+                    f"{self.eimer_path}/config/users.json"
+                )
                 user_roles_blob.upload_from_string(
                     data=json.dumps(users), content_type="application/json"
                 )
@@ -90,8 +165,23 @@ class EimerDBInstance:
         else:
             raise Exception("Cannot remove user. You are not an admin!")
 
-    def create_table(self, table_name, schema, partition_columns=None, editable=True):
-        if self.is_admin == True:
+    def create_table(
+        self, table_name, schema, partition_columns=None, editable=True
+    ):
+        """
+        Create a new table in EimerDB.
+
+        Args:
+            table_name (str): Name of the new table.
+            schema (str): JSON schema for the table.
+            partition_columns (list, optional): List of partition columns.
+            editable (bool, optional): Indicates if the table is editable.
+
+        Raises:
+            Exception: If the current user is not an admin.
+
+        """
+        if self.is_admin is True:
             token = AuthClient.fetch_google_credentials()
             client = storage.Client(credentials=token)
             bucket = client.bucket(self.bucket)
@@ -112,7 +202,9 @@ class EimerDBInstance:
             else:
                 new_table[table_name]["partition_columns"] = None
             tables.update(new_table)
-            tables_blob_blob = bucket.blob(f"{self.eimer_path}/config/tables.json")
+            tables_blob_blob = bucket.blob(
+                f"{self.eimer_path}/config/tables.json"
+            )
             tables_blob_blob.upload_from_string(
                 data=json.dumps(tables), content_type="application/json"
             )
@@ -120,13 +212,26 @@ class EimerDBInstance:
             raise Exception("Cannot create table. You are not an admin!")
 
     def main_table_insert(self, table_name, df):
+        """
+        Insert unedited data into a main table.
+
+        Args:
+            table_name (str): Name of the table to insert data into.
+            df (pandas.DataFrame): DataFrame containing the data to insert.
+
+        Raises:
+            Exception: If the current user is not an admin.
+
+        """
         if self.is_admin == True:
             token = AuthClient.fetch_google_credentials()
             client = storage.Client(credentials=token)
             bucket = client.bucket(self.bucket)
             json_data = self.tables[table_name]
             json_schema = json.dumps(json_data["schema"])
-            arrow_schema = arrow_schema_from_json(json.dumps(json_data["schema"]))
+            arrow_schema = arrow_schema_from_json(
+                json.dumps(json_data["schema"])
+            )
 
             table = pa.Table.from_pandas(df, schema=arrow_schema)
             fs = FileClient.get_gcs_file_system()
@@ -146,109 +251,142 @@ class EimerDBInstance:
             Exception("Cannot insert into main table. You are not an admin!")
 
     def query(self, sql_query, partition_select=None, unedited=False):
+        """
+        Execute an SQL query on an EimerDB table.
+
+        Args:
+            sql_query (str): SQL query to execute.
+            partition_select (dict): Dictionary specifying partition filters.
+            unedited (bool): Indicates whether to include unedited data.
+
+        Returns:
+            pandas.DataFrame: Resulting DataFrame from the query.
+
+        Raises:
+            Exception: If the table is not editable (for UPDATE queries).
+
+        """
         parsed_query = parse_sql_query(sql_query)
+        table_name = parsed_query["table_name"]
+        table_config = self.tables[table_name]
+        editable = table_config["editable"]
+        instance_name = self.eimerdb_name
         if parsed_query["operation"] == "SELECT":
             try:
                 columns = parsed_query["columns"]
-            except:
+            except Exception:
                 columns = None
-            if columns == ['*']:
+            if columns == ["*"]:
                 columns = None
-            table_name = parsed_query["table_name"]
             sql_filter = parsed_query["sql_filter"]
-
-            instance_name = self.eimerdb_name
-            table_config = self.tables[table_name]
             partitions = table_config["partition_columns"]
             bucket_name = table_config["bucket"]
-            editable = table_config["editable"]
             partitions_len = len(partitions)
             partition_levels = "**/" * partitions_len + "*"
             fs = FileClient.get_gcs_file_system()
-            table_files = fs.glob(f'gs://{bucket_name}/eimerdb/{instance_name}/{table_name}/{partition_levels}')
+            table_files = fs.glob(
+                f"gs://{bucket_name}/eimerdb/{instance_name}/{table_name}/{partition_levels}"
+            )
             if partition_select is not None:
                 filtered_files = []
                 for file in table_files:
-                    parts = file.split('/')
+                    parts = file.split("/")
 
                     all_matches = True
 
                     for key, values in partition_select.items():
-                        match_found = any(f"{key}={value}" in parts for value in values)
+                        match_found = any(
+                            f"{key}={value}" in parts for value in values
+                        )
 
                         if not match_found:
                             all_matches = False
                             break
-
                     if all_matches:
                         filtered_files.append(file)
                     table_files = filtered_files
             dataset = pq.read_table(table_files, filesystem=fs, columns=columns)
-            if sql_query and dataset:
-                sql_query = sql_query.replace(table_name, "dataset")
+            sql_query = sql_query.replace(f"FROM {table_name}", f"FROM dataset")
 
-                con = duckdb.connect()
-                df = con.execute(sql_query).df()
+            con = duckdb.connect()
+            df = con.execute(sql_query).df()
 
             if editable == True and unedited == False:
                 table_name_changes = table_name + "_changes"
-                table_files_changes = fs.glob(f'gs://{bucket_name}/eimerdb/{instance_name}/{table_name_changes}/{partition_levels}')
-                if partition_select is not None:
-                    filtered_files_changes = []
-                    for file in table_files_changes:
-                        parts = file.split('/')
+                table_files_changes = fs.glob(
+                    f"gs://{bucket_name}/eimerdb/{instance_name}/{table_name_changes}/{partition_levels}"
+                )
+                if len(table_files_changes) != 0:
+                    if partition_select is not None:
+                        filtered_files_changes = []
+                        for file in table_files_changes:
+                            parts = file.split("/")
 
-                        all_matches = True
+                            all_matches = True
 
-                        for key, values in partition_select.items():
-                            match_found = any(f"{key}={value}" in parts for value in values)
+                            for key, values in partition_select.items():
+                                match_found = any(
+                                    f"{key}={value}" in parts
+                                    for value in values
+                                )
 
-                            if not match_found:
-                                all_matches = False
-                                break
+                                if not match_found:
+                                    all_matches = False
+                                    break
+                        if all_matches:
+                            filtered_files_changes.append(file)
+                        table_files_changes = filtered_files_changes
 
-                    if all_matches:
-                        filtered_files_changes.append(file)
-                    table_files_changes = filtered_files_changes
-            
-                fs = FileClient.get_gcs_file_system()
-                dataset_changes = pq.read_table(table_files_changes, filesystem=fs, columns=columns)
-                if sql_query and dataset:
-                    sql_query = sql_query.replace("dataset", "dataset_changes")
-
+                    fs = FileClient.get_gcs_file_system()
+                    dataset_changes = pq.read_table(
+                        table_files_changes, filesystem=fs, columns=columns
+                    )
+                    sql_query = sql_query.replace(
+                        "dataset", "dataset_changes"
+                    )
                     con = duckdb.connect()
                     df_changes = con.execute(sql_query).df()
-                df_changes['datetime'] = pd.to_datetime(df_changes['datetime']).apply(lambda x: x.timestamp())
-                df_changes.sort_values('datetime', ascending=False, inplace=True)
-                df_changes.drop_duplicates(subset='uuid', keep='first', inplace=True)
-                
-                merged = pd.merge(df, df_changes, on='uuid', how='outer')
-                changed_rows = merged[merged['operation'].notna()]
-                df_cols = df.columns[1:]
+                    df_changes["datetime"] = pd.to_datetime(
+                        df_changes["datetime"]
+                    ).apply(lambda x: x.timestamp())
+                    df_changes.sort_values(
+                        "datetime", ascending=False, inplace=True
+                    )
+                    df_changes.drop_duplicates(
+                        subset="uuid", keep="first", inplace=True
+                    )
 
-                for index, row in changed_rows.iterrows():
-                    if row['operation'] == 'update':
-                        for col in df_cols:
-                            df.loc[df['uuid'] == row['uuid'], col] = row[col + '_y']
-                    elif row['operation'] == 'insert':
-                        if pd.isnull(row['uuid_x']):
-                            new_uuid = str(uuid4())
-                            new_row = {col: row[col + '_y'] for col in df_cols}
-                            new_row['uuid'] = new_uuid
-                            df = df.append(new_row, ignore_index=True)
-                    elif row['operation'] == 'delete':
-                        df = df[df['uuid'] != row['uuid']]
-                    elif row['operation'] == 'reset':
-                        pass
+                    merged = pd.merge(df, df_changes, on="uuid", how="outer")
+                    changed_rows = merged[merged["operation"].notna()]
+                    df_cols = df.columns[1:]
 
+                    for index, row in changed_rows.iterrows():
+                        if row["operation"] == "update":
+                            for col in df_cols:
+                                df.loc[df["uuid"] == row["uuid"], col] = row[
+                                    col + "_y"
+                                ]
+                        elif row["operation"] == "insert":
+                            if pd.isnull(row["uuid_x"]):
+                                new_uuid = str(uuid4())
+                                new_row = {
+                                    col: row[col + "_y"] for col in df_cols
+                                }
+                                new_row["uuid"] = new_uuid
+                                df = df.append(new_row, ignore_index=True)
+                        elif row["operation"] == "delete":
+                            df = df[df["uuid"] != row["uuid"]]
+                        elif row["operation"] == "reset":
+                            pass
             return df
-
         elif parsed_query["operation"] == "UPDATE":
+            if editable == False:
+                raise Exception(f"The table {table_name} is not editable!")
             try:
                 columns = parsed_query["columns"]
             except:
                 columns = None
-            if columns == ['*']:
+            if columns == ["*"]:
                 columns = None
             table_name = parsed_query["table_name"]
             where_clause = parsed_query["where_clause"]
@@ -260,42 +398,46 @@ class EimerDBInstance:
             partitions_len = len(partitions)
             partition_levels = "**/" * partitions_len + "*"
             fs = FileClient.get_gcs_file_system()
-            table_files = fs.glob(f'gs://{bucket_name}/eimerdb/{instance_name}/{table_name}/{partition_levels}')
+            table_files = fs.glob(
+                f"gs://{bucket_name}/eimerdb/{instance_name}/{table_name}/{partition_levels}"
+            )
             if partition_select is not None:
                 filtered_files = []
                 for file in table_files:
-                    parts = file.split('/')
+                    parts = file.split("/")
 
                     all_matches = True
 
                     for key, values in partition_select.items():
-                        match_found = any(f"{key}={value}" in parts for value in values)
+                        match_found = any(
+                            f"{key}={value}" in parts for value in values
+                        )
 
                         if not match_found:
                             all_matches = False
                             break
-
                     if all_matches:
                         filtered_files.append(file)
                     table_files = filtered_files
-            
             fs = FileClient.get_gcs_file_system()
             dataset = pq.read_table(table_files, filesystem=fs, columns=columns)
             con = duckdb.connect()
-            con.execute(f"CREATE TABLE updates AS FROM dataset WHERE {where_clause}")
-            sql_query = sql_query.replace(table_name, "updates")
+            con.execute(
+                f"CREATE TABLE updates AS FROM dataset WHERE {where_clause}"
+            )
+            sql_query = sql_query.replace(f"UPDATE {table_name}", f"UPDATE updates")
             con.execute(sql_query)
             df = con.table("updates").df()
             df["user"] = get_initials()
             df["datetime"] = get_datetime()
             df["operation"] = "update"
-            
+
             table_path = self.tables[table_name]["table_path"] + "_changes"
             fs = FileClient.get_gcs_file_system()
             update_table = pa.Table.from_pandas(df)
-            
+
             uuid = uuid4()
-            filename = f'commit_{uuid}_{{i}}'
+            filename = f"commit_{uuid}_{{i}}"
 
             pq.write_to_dataset(
                 update_table,
@@ -303,5 +445,5 @@ class EimerDBInstance:
                 partition_cols=partitions,
                 basename_template=filename,
                 filesystem=fs,
-                )
+            )
             print("Data successfully updated!")
