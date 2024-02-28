48e49d37770e268087ac3d992c2d5d4f03c680ea
StianElisenberg <stian.elisenberg@ssb.no>
2024-02-28 23:57:11 +0100

    Removed support for python version 3.9. Added support for version 3.12.

--- noxfile.py
+++ noxfile.py
@@ -23,7 +23,7 @@ except ImportError:
     raise SystemExit(dedent(message)) from None
 
 package = "eimerdb"
-python_versions = ["3.10", "3.11", "3.9"]
+python_versions = ["3.10", "3.11", "3.12"]
 nox.needs_version = ">= 2021.6.6"
 nox.options.sessions = (
     "pre-commit",
