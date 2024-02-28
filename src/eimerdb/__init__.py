69c27852a3214f6cae8c23257760b86b45e4f9c5
StianElisenberg <stian.elisenberg@ssb.no>
2024-02-28 21:56:26 +0100

    changed default python version

--- src/eimerdb/__init__.py
+++ src/eimerdb/__init__.py
@@ -3,6 +3,7 @@
 from .functions import create_eimerdb
 from .instance import EimerDBInstance
 
+
 __all__ = [
     "create_eimerdb",
     "EimerDBInstance",
--- src/eimerdb/__main__.py
+++ src/eimerdb/__main__.py
@@ -3,6 +3,7 @@
 from eimerdb.functions import main as functions_main
 from eimerdb.instance import main as instance_main
 
+
 if __name__ == "__main__":
     functions_main()
     instance_main()
