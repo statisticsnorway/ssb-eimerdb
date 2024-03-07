f71d32b8b27cb6c2c7ff41645223a109951701ad
Roar Skinderviken <roar.skinderviken@gmail.com>
2024-03-07 20:38:29 +0100

    Added return type for tests

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -6,12 +6,12 @@ from eimerdb.functions import get_datetime
 from eimerdb.functions import get_initials
 
 
-def test_get_datetime():
+def test_get_datetime() -> None:
     assert get_datetime().startswith(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
 
 
 @patch("eimerdb.functions.AuthClient.fetch_local_user_from_jupyter")
-def test_get_initials_given_mocked_auth_client(auth_client_mock: Mock):
+def test_get_initials_given_mocked_auth_client(auth_client_mock: Mock) -> None:
     auth_client_mock.return_value = {"username": "user42@some.org"}
 
     assert get_initials() == "user42"
@@ -19,5 +19,5 @@ def test_get_initials_given_mocked_auth_client(auth_client_mock: Mock):
     auth_client_mock.assert_called_once()
 
 
-def test_get_initials_given_no_auth_client():
+def test_get_initials_given_no_auth_client() -> None:
     assert get_initials() == "user"
