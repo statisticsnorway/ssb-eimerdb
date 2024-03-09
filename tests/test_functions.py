6c7b146fdee18a3d06a88ab871c68f91e6eb8906
Roar Skinderviken <roar.skinderviken@gmail.com>
2024-03-09 19:12:18 +0100

    Reformatted file

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -20,7 +20,7 @@ class TestFunctions(unittest.TestCase):
         # Check if the string matches the expected format
         self.assertRegex(result, r"\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d+")
 
-    @patch('eimerdb.functions.AuthClient.fetch_local_user_from_jupyter')
+    @patch("eimerdb.functions.AuthClient.fetch_local_user_from_jupyter")
     def test_get_initials_with_mock(self, mock_fetch: Mock) -> None:
         mock_fetch.return_value = {"username": "john.doe@example.com"}
         result = get_initials()
@@ -30,8 +30,8 @@ class TestFunctions(unittest.TestCase):
         result = get_initials()
         self.assertEqual(result, "user")
 
-    @patch('eimerdb.functions.AuthClient.fetch_google_credentials')
-    @patch('google.cloud.storage.Client')
+    @patch("eimerdb.functions.AuthClient.fetch_google_credentials")
+    @patch("google.cloud.storage.Client")
     def test_get_json(self, mock_client: Mock, mock_fetch: Mock) -> None:
         mock_fetch.return_value = "token"
 
@@ -50,27 +50,31 @@ class TestFunctions(unittest.TestCase):
         json_schema = [
             {"name": "field1", "type": "int8", "label": "Field 1"},
             {"name": "field2", "type": "string", "label": "Field 2"},
-            {"name": "field3", "type": "timestamp(us)", "label": "Field 3"}
+            {"name": "field3", "type": "timestamp(us)", "label": "Field 3"},
         ]
         result = arrow_schema_from_json(json_schema)
 
-        assert result == pa.schema([
-            pa.field("field1", pa.int8(), metadata={"label": "Field 1"}),
-            pa.field("field2", pa.string(), metadata={"label": "Field 2"}),
-            pa.field("field3", pa.timestamp("us"), metadata={"label": "Field 3"})
-        ])
+        assert result == pa.schema(
+            [
+                pa.field("field1", pa.int8(), metadata={"label": "Field 1"}),
+                pa.field("field2", pa.string(), metadata={"label": "Field 2"}),
+                pa.field("field3", pa.timestamp("us"), metadata={"label": "Field 3"}),
+            ]
+        )
 
     def test_parse_sql_query(self) -> None:
         sql_query = "SELECT * FROM table WHERE condition"
         result = parse_sql_query(sql_query)
-        assert result == {'columns': ['*'],
-                          'operation': 'SELECT',
-                          'select_clause': '',
-                          'table_name': ['table'],
-                          'where_clause': 'condition'}
+        assert result == {
+            "columns": ["*"],
+            "operation": "SELECT",
+            "select_clause": "",
+            "table_name": ["table"],
+            "where_clause": "condition",
+        }
 
-    @patch('eimerdb.functions.AuthClient.fetch_google_credentials')
-    @patch('google.cloud.storage.Client')
+    @patch("eimerdb.functions.AuthClient.fetch_google_credentials")
+    @patch("google.cloud.storage.Client")
     def test_create_eimerdb(self, mock_client: Mock, mock_fetch: Mock) -> None:
         mock_fetch.return_value = "token"
         mock_blob = MagicMock()
@@ -82,5 +86,5 @@ class TestFunctions(unittest.TestCase):
         create_eimerdb("bucket_name", "db_name")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
