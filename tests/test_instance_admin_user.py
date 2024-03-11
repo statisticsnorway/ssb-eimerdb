73a567c9f644ddfe8a106f528f5b1bfb0d706faf
Roar Skinderviken <roar.skinderviken@gmail.com>
2024-03-11 20:00:14 +0100

    Added test__query_delete #3

--- tests/test_instance_admin_user.py
+++ tests/test_instance_admin_user.py
@@ -399,7 +399,7 @@ class TestEimerDBInstanceAdminUser(unittest.TestCase):
 
     @patch("eimerdb.instance.FileClient.get_gcs_file_system")
     @patch("eimerdb.instance.duckdb.connect")
-    def test_query_delete(self, mock_connect: Mock, _: Mock) -> None:
+    def test__query_delete(self, mock_connect: Mock, _: Mock) -> None:
         # Mocking objects
         mock_fs = MagicMock()
         mock_connect.return_value = MagicMock()
