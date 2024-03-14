5d09713f41757113a191f88fe16a7f8c8eddf71d
Roar Skinderviken <roar.skinderviken@gmail.com>
2024-03-14 10:12:51 +0100

    Fixed test

--- tests/test_query.py
+++ tests/test_query.py
@@ -113,6 +113,17 @@ class TestGetPartitionedFiles(unittest.TestCase):
     def test_update_pyarrow_table(self):
         # Create the original PyArrow table
         original_schema = pa.schema(
+            [pa.field("row_id", pa.string()), pa.field("value", pa.int32())]
+        )
+
+        original_data = [
+            pa.array(["id1", "id2", "id3"]),
+            pa.array([1, 2, 3]),
+        ]
+        original_table = pa.Table.from_arrays(original_data, schema=original_schema)
+
+        # Create the changes PyArrow table
+        changes_schema = pa.schema(
             [
                 pa.field("row_id", pa.string()),
                 pa.field("value", pa.int32()),
@@ -121,29 +132,9 @@ class TestGetPartitionedFiles(unittest.TestCase):
                 pa.field("user", pa.string()),
             ]
         )
-
-        original_data = [
-            pa.array(["id1", "id2", "id3"]),
-            pa.array([1, 2, 3]),
-            pa.array(
-                [1640995200000000000, 1640995200000000000, 1640995200000000000]
-            ),  # Timestamps in nanoseconds
-            pa.array(["update", "update", "delete"]),
-            pa.array(["user1", "user2", "user3"]),
-        ]
-        original_table = pa.Table.from_arrays(original_data, schema=original_schema)
-
-        # Create the changes PyArrow table
-        changes_schema = pa.schema(
-            [
-                pa.field("row_id", pa.string()),
-                pa.field("datetime", pa.timestamp("ns")),
-                pa.field("operation", pa.string()),
-                pa.field("user", pa.string()),
-            ]
-        )
         changes_data = [
             pa.array(["id2", "id3", "id4"]),
+            pa.array([1, 2, 3]),
             pa.array(
                 [1640995200000000000, 1640995200000000000, 1640995200000000000]
             ),  # Timestamps in nanoseconds
@@ -155,27 +146,22 @@ class TestGetPartitionedFiles(unittest.TestCase):
         # Call the function under test
         update_pyarrow_table(original_table, changes_table)
 
-        # Stian: Kommenter inn resten
-        # updated_table = update_pyarrow_table(original_table, changes_table)
+        updated_table = update_pyarrow_table(original_table, changes_table)
 
         # Assert the expected output
 
-        # expected_schema = pa.schema([
-        #     pa.field("row_id", pa.string()),
-        #     pa.field("value", pa.int32()),
-        #     pa.field("datetime", pa.timestamp("ns")),
-        #     pa.field("operation", pa.string()),
-        #     pa.field("user", pa.string())
-        # ])
-        #
-        # expected_data = [
-        #     pa.array(["id1", "id2", "id4"]),
-        #     pa.array([1, 2, None]),  # Value of 'id3' deleted
-        #     pa.array([1640995200000000000, 1640995200000000000, 1640995200000000000]),  # Updated timestamps
-        #     pa.array(["update", "update", "update"]),  # Deleted 'delete' operation
-        #     pa.array(["user1", "user2", "user4"])  # Updated user
-        # ]
-        # expected_output = pa.Table.from_arrays(expected_data, schema=expected_schema)
-        #
-        # # Assert the table contents are equal
-        # self.assertTrue(updated_table.equals(expected_output))
+        expected_schema = pa.schema(
+            [
+                pa.field("row_id", pa.string()),
+                pa.field("value", pa.int32()),
+            ]
+        )
+
+        expected_data = [
+            pa.array(["id1", "id2", "id4"]),
+            pa.array([1, 1, 3]),  # Value of 'id3' deleted
+        ]
+        expected_output = pa.Table.from_arrays(expected_data, schema=expected_schema)
+
+        # Assert the table contents are equal
+        self.assertTrue(updated_table.equals(expected_output))
