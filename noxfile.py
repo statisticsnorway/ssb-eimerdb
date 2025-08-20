ba32209ef941bb16e7ab60232a56143916f13bef
Arne Sørli <81353974+arneso-ssb@users.noreply.github.com>
2025-08-20 10:36:49 +0200

    Update to template version 2025.7.18

--- noxfile.py
+++ noxfile.py
@@ -23,7 +23,8 @@ except ImportError:
     raise SystemExit(dedent(message)) from None
 
 package = "eimerdb"
-python_versions = ["3.10", "3.11", "3.12"]
+python_versions = ["3.11", "3.12", "3.13"]
+python_versions_for_test = python_versions + ["3.10"]
 nox.needs_version = ">= 2021.6.6"
 nox.options.sessions = (
     "pre-commit",
@@ -150,7 +151,7 @@ def mypy(session: Session) -> None:
         session.run("mypy", f"--python-executable={sys.executable}", "noxfile.py")
 
 
-@session(python=python_versions)
+@session(python=python_versions_for_test)
 def tests(session: Session) -> None:
     """Run the test suite."""
     session.install(".")
