9067ce6f357e4e3ccab5ecd24aa22fb8eac7456b
Jacob Andreas Osnes <osn@ssb.no>
2025-11-18 12:54:03 +0100

    Change coverage session to use second Python version

--- noxfile.py
+++ noxfile.py
@@ -171,7 +171,7 @@ def tests(session: Session) -> None:
             session.notify("coverage", posargs=[])
 
 
-@session(python=python_versions[0])
+@session(python=python_versions[1])
 def coverage(session: Session) -> None:
     """Produce the coverage report."""
     args = session.posargs or ["report", "--skip-empty"]
