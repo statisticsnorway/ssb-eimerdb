80986b6ea8fd257a874accdbf235e74842fc0a32
Jacob Andreas Osnes <osn@ssb.no>
2025-11-18 13:16:16 +0100

    Remove specific Python version from coverage session

--- noxfile.py
+++ noxfile.py
@@ -171,7 +171,7 @@ def tests(session: Session) -> None:
             session.notify("coverage", posargs=[])
 
 
-@session(python=python_versions[1])
+@session()
 def coverage(session: Session) -> None:
     """Produce the coverage report."""
     args = session.posargs or ["report", "--skip-empty"]
